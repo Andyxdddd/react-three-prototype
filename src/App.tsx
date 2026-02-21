b8c21808b2e0acee3f81ebdfd6d3b7f99038ab21
Andyxdddd <badea.ulrich@gmail.com>
2026-02-21 10:41:18 +0100

    fix: adjust camera position and lighting in Canvas

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@ const App = () => {
   return (
     <>
       <div className="bg-gray-950 h-screen">
-        <Canvas camera={{ position: [2, 2, 10] }}>
+        <Canvas camera={{ position: [5, 5, 10] }}>
           <mesh position={[0, 0, 0]}>
             <boxGeometry args={[2, 2, 2]} />
             <meshPhongMaterial />
           </mesh>
-          <ambientLight intensity={0.1} />
-          <directionalLight position={[0, 0, 5]} color="red" />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[1, 1, 0]} color="white" />
         </Canvas>
       </div>
     </>
