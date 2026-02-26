99764355476a9e13b698368d6e70430deb9db706
Andyxdddd <badea.ulrich@gmail.com>
2026-02-26 18:36:58 +0100

    feat(world): add extra props (hardcoded)

--- src/App.tsx
+++ src/App.tsx
@@ -27,23 +27,51 @@ const World = () => {
     <>
       <Sky />
       <ambientLight intensity={0.5} />
-      <directionalLight position={[1, 1, 0]} color="white" />
+      <directionalLight position={[1, 1, 0]} />
+
       <mesh position={[0, 0, 0]}>
         <boxGeometry args={[12, 1, 12]} />
-        <meshPhongMaterial />
+        <meshPhongMaterial color="lightgreen" />
       </mesh>
+
+      <mesh position={[0, 0, 12]}>
+        <boxGeometry args={[12, 1, 12]} />
+        <meshPhongMaterial color="lightgreen" />
+      </mesh>
+
       <mesh position={[9, 0, 0]}>
         <boxGeometry args={[6, 1, 12]} />
-        <meshPhongMaterial color="blue" />
+        <meshPhongMaterial color="gray" />
       </mesh>
+
+      <mesh position={[9, 0, 12]}>
+        <boxGeometry args={[6, 1, 12]} />
+        <meshPhongMaterial color="gray" />
+      </mesh>
+
       <mesh position={[18, 0, 0]}>
         <boxGeometry args={[12, 1, 12]} />
-        <meshPhongMaterial />
+        <meshPhongMaterial color="lightgreen" />
+      </mesh>
+
+      <mesh position={[18, 0, 12]}>
+        <boxGeometry args={[12, 1, 12]} />
+        <meshPhongMaterial color="lightgreen" />
       </mesh>
 
       <mesh position={[6, 1, -1]}>
         <boxGeometry args={[0.1, 3, 0.1]} />
         <meshPhongMaterial color="orange" />
+      </mesh>
+
+      <mesh position={[15, 1, -4]}>
+        <boxGeometry args={[4, 4, 4]} />
+        <meshPhongMaterial color="red" />
+      </mesh>
+
+      <mesh position={[7.5, 1, 8]}>
+        <boxGeometry args={[2, 1.5, 4]} />
+        <meshPhongMaterial color="blue" />
       </mesh>
     </>
   );
